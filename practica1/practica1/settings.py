e09b49001ed52725d961cb2c3faebaf31a52dc26
lmdcma27 <42389361+lmdcma27@users.noreply.github.com>
2019-06-05 10:51:51 -0600

    Update settings.py

--- practica1/practica1/settings.py
+++ practica1/practica1/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'practica1.urls'
 TEMPLATES = [   
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'practica1/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'apps/registro/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
